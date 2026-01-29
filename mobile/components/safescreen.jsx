2376e4c98c893b7a97e20960322e7b03f58258f8
Yasith Inosh <yasithinosh1@gmail.com>
2026-01-29 19:51:55 +0530

    create safe screen

---
+++ mobile/components/safescreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const safescreen = () => {
+  return (
+    <View>
+      <Text>safescreen</Text>
+    </View>
+  )
+}
+
+export default safescreen
